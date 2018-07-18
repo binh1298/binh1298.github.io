c7631c9ebf32f9caaad801ca063c26f65ce555a0
Binh <binh1298@gmail.com>
2018-07-18 20:01:10 +0700

    Update visibility for other characters

--- js/others.js
+++ js/others.js
@@ -1,17 +1,27 @@
-var othersDescription = [
-  "Mary is a human Devil Hunter who first appeared in Devil May Cry 3: Dante's Awakening as both a supporting character and boss. She is the daughter of Arkham and sought to hunt him down, encountering Dante in the process. After the events, she joins Devil May Cry.",
-  "Vergil was the eldest of twin sons born of the demon Sparda and human Eva. Following the death of their mother as children, Vergil and Dante go their separate ways, with Vergil rejecting his humanity and embracing his demonic heritage, contrasting his younger brother's embrace of humanity.",
-  "Trish is a demon created by Mundus who strangely resembles Dante's mother, Eva. After his defeat by the hands of Dante, she later joined Devil May Cry and became a devil hunter alongside him. During her stint within the Order of the Sword, she went under the alias \"Gloria\""
-]
 function changeOthersDescription(des){
+  var othersDescription = [
+    "Mary is a human Devil Hunter who first appeared in Devil May Cry 3: Dante's Awakening as both a supporting character and boss. She is the daughter of Arkham and sought to hunt him down, encountering Dante in the process. After the events, she joins Devil May Cry.",
+    "Vergil was the eldest of twin sons born of the demon Sparda and human Eva. Following the death of their mother as children, Vergil and Dante go their separate ways, with Vergil rejecting his humanity and embracing his demonic heritage, contrasting his younger brother's embrace of humanity.",
+    "Trish is a demon created by Mundus who strangely resembles Dante's mother, Eva. After his defeat by the hands of Dante, she later joined Devil May Cry and became a devil hunter alongside him. During her stint within the Order of the Sword, she went under the alias \"Gloria\""
+  ]
+  var modelIDs = [
+    document.getElementById("marry-model"),
+    document.getElementById("vergil-model"),
+    document.getElementById("trish-model")
+  ]
   var description = document.getElementById("others-description").children[0];
   if(description.id == des) return;
   description.style.opacity = "0";
+  for(var i = 0; i < 3; i++)
+  {
+    if(i != des) modelIDs[i].style.opacity = "0.4";
+  }
   setTimeout(
     function setOpacity(){
       description.innerHTML = othersDescription[des];
       description.style.opacity = "1";
       description.id = des;
+      modelIDs[des].style.opacity = "1";
     },
     500
   );
