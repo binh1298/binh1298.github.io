a75a4d8bda8c2c9eaae5925cb5c587ed55530c69
binh1298 <binh1298@gmail.com>
2018-07-22 15:09:14 +0700

    Update resources for dante and nero sections

--- js/weapons.js
+++ js/weapons.js
@@ -20,6 +20,9 @@ function changeWeaponHudClockwise(obj){
   containerDanteAction[0].id = containerDanteAction[1].id;
   containerDanteAction[1].id = containerDanteAction[2].id;
   containerDanteAction[2].id = temp;
+  containerDanteAction[0].currentTime = "0";
+  containerDanteAction[1].currentTime = "0";
+  containerDanteAction[2].currentTime = "0";
 }
 function changeWeaponHudCounterClockwise(obj){
   containerHUD = obj.children;
